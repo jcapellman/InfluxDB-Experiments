7b32c226c14788d12388f39238b6a467c8f67768
Jarred Capellman <jcapellman@hotmail.com>
2022-07-04 07:39:31 -0500

    Added: WriteAsync and QueryRawString methods

--- InfluxDB.lib/InfluxDBContext.cs
+++ InfluxDB.lib/InfluxDBContext.cs
@@ -1,5 +1,5 @@
 ﻿using InfluxDB.Client;
-
+using InfluxDB.Client.Api.Domain;
 using Microsoft.Extensions.Logging;
 
 namespace InfluxDBShim.lib
@@ -24,7 +24,7 @@ namespace InfluxDBShim.lib
             return true;
         }
 
-        public async Task<List<InfluxDB.Client.Core.Flux.Domain.FluxRecord>> QueryAsync<FluxRecord>(string query)
+        public async Task<List<T>> QueryAsync<T>(string query)
         {
             var queryApi = _client?.GetQueryApi();
 
@@ -33,19 +33,33 @@ namespace InfluxDBShim.lib
                 throw new InvalidOperationException("QueryAPI was null");
             }
 
-            var tables = await queryApi.QueryAsync(query);
+            return await queryApi.QueryAsync<T>(query);
+        }
 
-            var records = new List<InfluxDB.Client.Core.Flux.Domain.FluxRecord>();
+        public async Task<string> QueryRawStringAsync<T>(string query)
+        {
+            var queryApi = _client?.GetQueryApi();
 
-            tables.ForEach(table =>
+            if (queryApi == null)
             {
-                table.Records.ForEach(record =>
-                {
-                    records.Add(record);                    
-                });
-            });
+                throw new InvalidOperationException("QueryAPI was null");
+            }
 
-            return records;
+            return await queryApi.QueryRawAsync(query);
+        }
+
+        public async Task<bool> WriteAsync<T>(T newObject, WritePrecision precision = WritePrecision.Ns)
+        {
+            var writeApi = _client?.GetWriteApiAsync();
+
+            if (writeApi == null)
+            {
+                throw new InvalidOperationException("writeApi was null");
+            }
+
+            await writeApi.WriteMeasurementAsync(newObject, precision, "bucket_name", "org_id");
+            
+            return true;
         }
 
         public void Dispose()
